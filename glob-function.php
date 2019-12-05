de92584995787055547c005488c421efb9ed7daa
Md. Sumon Islam <sumonst21@gmail.com>
2019-12-06 01:36:25 +0600

    Update glob-function.php

--- glob-function.php
+++ glob-function.php
@@ -1,26 +1,25 @@
 <?php
 /**
- * find files matching a pattern
- * using PHP "glob" function and recursion
- *
- * @return array containing all pattern-matched files
- *
- * @param string $dir     - directory to start with
- * @param string $pattern - pattern to glob for
- */
+ * find files matching a pattern
+ * using PHP "glob" function and recursion
+ *
+ * @return array containing all pattern-matched files
+ *
+ * @param string $dir     - directory to start with
+ * @param string $pattern - pattern to glob for
+ */
 function find($dir, $pattern){
-    // escape any character in a string that might be used to trick
-    // a shell command into executing arbitrary commands
-    $dir = escapeshellcmd($dir);
-    // get a list of all matching files in the current directory
-    $files = glob("$dir/$pattern");
-    // find a list of all directories in the current directory
-    // directories beginning with a dot are also included
-    foreach (glob("$dir/{.[^.]*,*}", GLOB_BRACE|GLOB_ONLYDIR) as $sub_dir){
-        $arr   = find($sub_dir, $pattern);  // resursive call
-        $files = array_merge($files, $arr); // merge array with files from subdirectory
-    }
-    // return all found files 
-    return $files;
+    // escape any character in a string that might be used to trick
+    // a shell command into executing arbitrary commands
+    $dir = escapeshellcmd($dir);
+    // get a list of all matching files in the current directory
+    $files = glob("$dir/$pattern");
+    // find a list of all directories in the current directory
+    // directories beginning with a dot are also included
+    foreach (glob("$dir/{.[^.]*,*}", GLOB_BRACE|GLOB_ONLYDIR) as $sub_dir){
+        $arr   = find($sub_dir, $pattern);  // resursive call
+        $files = array_merge($files, $arr); // merge array with files from subdirectory
+    }
+    // return all found files 
+    return $files;
 }
-?>
